f966380abafbd0a31ca530f4faf3c4817e785698
Cleydson <cleydson.cg@live.com>
2022-03-12 00:22:25 -0300

    Update index.tsx

--- src/components/painelLayout/index.tsx
+++ src/components/painelLayout/index.tsx
@@ -1,23 +1,15 @@
 import MyHeader from "components/header";
 import { useAuth } from "contexts/AuthContext";
-import { useEffect } from "react";
 import { Container } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
-import { toast } from "react-toastify";
+import { Navigate, Outlet } from "react-router-dom";
 
 
 
 const PainelLayout = () => { 
-    const {isAuthenticated, hasAnyRoles, signOut} = useAuth();
+    const {isAuthenticated} = useAuth();
     
-    useEffect(() => {
-        if(!isAuthenticated || hasAnyRoles(['ROLE_USER'])){
-            toast.error('Você não tem permissão para acessar esta página');
-            signOut();
-        }
-    } , [isAuthenticated, hasAnyRoles, signOut]);
 
-    return (
+    return !isAuthenticated() ? <Navigate to="/auth/login" /> : (
         <>  
             <MyHeader />
             <Container fluid className="mt-4 px-5 mb-5">
